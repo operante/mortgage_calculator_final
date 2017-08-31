b9f195f29a1c5ea25a17edcc5c81d8e7b5144e4b
operante <taku.haneda@gmail.com>
2017-08-31 11:58:09 -0700

    Input validation for Purchase Price input.

--- js/mortgageFunctions.js
+++ js/mortgageFunctions.js
@@ -1,30 +1,44 @@
+/* computeLoan()
+ *
+ * This function is invoked when Calculate Payment button is clicked to read the purchase price, loan term, and other
+ * details to simulate and display the Monthly payment, Total Payment, Total Interest.
+ *
+ */
 function computeLoan() {
-    let purchasePrice = document.getElementById("amount").value;
-    let interestRate = document.getElementById("rate").value * 0.01;
-    let numbersMonths = document.getElementById("months").value * 12;
-    let downPercent = document.getElementById("downPercent").value * 0.01;
-    let downPayment = purchasePrice * downPercent;
-    let loanPrincipal = purchasePrice - downPayment;
-    let monthlyPayment = new MonthlyPayment(loanPrincipal, numbersMonths, interestRate); //monthly total payment
-    let totalEverything = monthlyPayment.payment * numbersMonths;
-    let totalInterest = totalEverything - loanPrincipal;
+    while (true) {
+        if (document.getElementById("purchasePrice").value === "") {
+            document.getElementById("validateAmountMsg").innerHTML = "Purchase price is required.";
+        } else {
+            let purchasePrice = document.getElementById("purchasePrice").value;
+            let interestRate = document.getElementById("rate").value * 0.01;       // % converted to decimal
+            let numbersMonths = document.getElementById("months").value * 12;      // Years converted to numbers of month
+            let downPercent = document.getElementById("downPercent").value * 0.01; // % converted to decimal
+            let downPayment = purchasePrice * downPercent;                         // Downpayment in $
+            let loanPrincipal = purchasePrice - downPayment;
+            let monthlyPayment = new MonthlyPayment(loanPrincipal, numbersMonths, interestRate); //monthly total payment
+            let totalEverything = monthlyPayment.payment * numbersMonths;
+            let totalInterest = totalEverything - loanPrincipal;
 
-    document.getElementById("downpayment").innerHTML = "$" + downPayment;
-    document.getElementById('newPayment').innerHTML = "Monthly Payment = $" + monthlyPayment.payment;
-    document.getElementById('monthly').innerHTML = "Your monthly total payment is: " + "$" + monthlyPayment.payment;
-    document.getElementById('total').innerHTML = "Your total payment: " + "$" + totalEverything.toFixed(2);
-    document.getElementById('interest').innerHTML = "Your interest amount is: " + "$" + totalInterest.toFixed(2);
+            document.getElementById("downpayment").innerHTML = "$" + downPayment;
+            document.getElementById('newPayment').innerHTML = "Your Monthly Payment = $" + monthlyPayment.payment;
+            document.getElementById('monthly').innerHTML = "Your monthly total payment is: " + "$" + monthlyPayment.payment;
+            document.getElementById('total').innerHTML = "Your total payment: " + "$" + totalEverything.toFixed(2);
+            document.getElementById('interest').innerHTML = "Your interest amount is: " + "$" + totalInterest.toFixed(2);
+
+            requestData(document.getElementById('purchasePrice').value);
+        }
+        break;
+    }
 }
 
-/* Calculate Monthly Payment, used in both User the specified rate as well as lender-term specific rates
- *
+/* MonthlyPayment(loanPrincipal, numberOfMonths, rate)
+Calculate Monthly Payment, used in both User the specified rate as well as lender-term specific rates
  * M = P * (r(1+r)^n / (1+r)^n - 1)
  * M: Monthly Payment
  * P: Principal
- * r: monthly interest rate, calculated by dividing your annual interest rate by 12.
- * n: number of payments (the number of months you will be paying the loan)[6]
+ * r: monthly interest rate, annual interest rate divided by 12.
+ * n: number of payments
  */
-
 function MonthlyPayment (loanPrincipal, numberOfMonths, rate) {
     let monthlyRate = (rate/12);         // This is r.
     let payment = loanPrincipal *
@@ -32,23 +46,29 @@ function MonthlyPayment (loanPrincipal, numberOfMonths, rate) {
     this.payment = payment.toFixed(2).toString();
 }
 
-
-function RequestData(loanPrincipal){
+/* requestData(loadPrincipal)
+ * Called by computeLoan().
+ * Get mortgate lender data.
+ */
+function requestData(loanPrincipal){
     let f = new XMLHttpRequest();
     let html = "";
     f.open('GET', './data/data.json', true);
     f.send();
-    f.onreadystatechange = function() { // (3)
+    f.onreadystatechange = function() {
         if (f.readyState != 4) return;
         if (f.status != 200) {
             console.log(f.status + ': ' + f.statusText);
         } else {
             generateInnerHTML(f, html, loanPrincipal);
-            }
         }
+    }
 }
 
-
+/* generateInnerHTML(f, html, loanPrincipal)
+ * Called by requestData().
+ * Generate lender list HTML and insert into the page.
+ */
 function generateInnerHTML(f, html, loanPrincipal) {
     let loanTerm = 360;
     let dataArray = JSON.parse(f.responseText);
@@ -65,5 +85,16 @@ function generateInnerHTML(f, html, loanPrincipal) {
             'Lender Phone No.: ' + phone + '<br>' +
             'Lender Website: ' + webUrl + '<br><br>';
         document.getElementById('lender-list').innerHTML = html;
+    }
+}
+
+/*
+ * Input validations
+ */
+function validateNumericInput() {
+    if (document.getElementById("purchasePrice").value) {
+        document.getElementById("validateAmountMsg").innerHTML = "";
+    } else {
+        document.getElementById("validateAmountMsg").innerHTML = "Please enter only numbers.";
     }
 }
